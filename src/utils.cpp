e1d8fb7f38bfe8a2bfda4b87fae814072fdc1aeb
fox102447@gmail.com <fox102447@gmail.com>
2025-07-10 19:31:54 +0300

    added readFromFile function

--- src/utils.cpp
+++ src/utils.cpp
@@ -29,6 +29,17 @@ int findCloseBrace(const vector_of_pairs& vec, size_t index) {
 	return (iter != vec.end()) ? iter->second : -1;
 }
 
+std::string readFromFile(const char* file_name) {
+	std::string code;
+	std::ifstream file(file_name);
+	if (file.is_open()) {
+		std::string line;
+		while (std::getline(file, line)) code += line;
+	}
+	file.close();
+	return code;
+}
+
 uint8_t getDigit(const std::string& code, size_t& pos) {
 	std::string temp;
 	while (std::isdigit(code[pos]) && code[pos] != ')') temp += code[pos++];
--- src/utils.hpp
+++ src/utils.hpp
@@ -6,6 +6,7 @@
 #include <stack>
 #include <vector>
 #include <algorithm>
+#include <fstream>
 
 using vector_of_pairs = std::vector<std::pair<int, int>>;
 
@@ -13,4 +14,5 @@ vector_of_pairs getBraces(const std::string& text);
 int findOpenBrace(const vector_of_pairs& vec, size_t index);
 int findCloseBrace(const vector_of_pairs& vec, size_t index);
 uint8_t getDigit(const std::string& code, size_t& pos);
+std::string readFromFile(const char* file_name);
 #endif // !UTILS_HPP
