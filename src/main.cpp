799c97dde872a7c204c93b79c50b02569661c534
fox102447@gmail.com <fox102447@gmail.com>
2025-07-10 19:30:56 +0300

    Fixed bugs

--- src/main.cpp
+++ src/main.cpp
@@ -12,33 +12,43 @@
 */
 
 // 72 101 108 108 111 44 32 87 111 114 108 100 33 -> Hello, World!
-// +[(72){.}(101){.}(108){..}(111){.}+]+[(44){,!}+]+[(32){.}(87){.}(111){.}(114){.}------.+++++++]+[(33){.!}+]
+// +[(72){.}(101){.}(108){..}(111){.}+]+[(44){,!}+]+[(32){.}(87){.}(111){.}(114){.}+]+[(108){.}+]+[(100){.}+]+[(33){.!}+]
 
 /* MAIN */
-int main(int argc, char* argv) {
+int main(int argc, char* argv[]) {
+	if (argv[1] == NULL) {
+		std::cerr << "BrokenEyes file_name.be";
+		exit(1);
+	}
 	uint8_t value = 0;
-	std::string code = "+[(72){.}(101){.}(108){..}(111){.}+]+[(44){,!}+]+[(32){.}(87){.}(111){.}(114){.}+]+[(108){.}+]+[(100){.}+]+[(33){.!}+]";
+	std::string code = readFromFile(argv[1]);
+	std::cout << code << std::endl;
+	std::remove_if(code.begin(), code.end(), isspace); // Remove all space
 	vector_of_pairs braces = getBraces(code);
 	std::stack<int> opened_loop_pos;
 	for (size_t i = 0; i < code.size(); ++i) {
-		if (code[i] == '!') {
-			i = opened_loop_pos.top() + 1;
-			opened_loop_pos.pop();
-		}
-		if (code[i] == '+') ++value;
-		if (code[i] == '-') --value;
-		if (code[i] == '.') std::cout << static_cast<char>(value);
-		if (code[i] == ',') std::cout << static_cast<char>(value) << " ";
-		if (code[i] == ';') std::cout << static_cast<char>(value) << std::endl;
-		if (code[i] == '[') {
-			if (value == 0) i = findCloseBrace(braces, i);
-			else opened_loop_pos.push(findCloseBrace(braces, i));
-		} else if (code[i] == ']')
-			if (value != 0) i = findOpenBrace(braces, i);
-		if (code[i] == '(') {
-			if (value != getDigit(code, ++i)) {
-				i = findCloseBrace(braces, ++i);
-			}
+		switch (code[i]) {
+			case '!':
+				i = opened_loop_pos.top() + 1;
+				opened_loop_pos.pop();
+				break;
+			case '+': ++value; break;
+			case '-': --value; break;
+			case '.': std::cout << static_cast<char>(value); break;
+			case ',': std::cout << static_cast<char>(value) << " "; break;
+			case ';': std::cout << static_cast<char>(value) << std::endl; break;
+			case '[':
+				if (value == 0) i = findCloseBrace(braces, i);
+				else opened_loop_pos.push(findCloseBrace(braces, i));
+				break;
+			case ']':
+				if (value != 0) i = findOpenBrace(braces, i);
+				break;
+			case '(':
+				if (value != getDigit(code, ++i)) {
+					i = findCloseBrace(braces, ++i);
+				}
+				break;
 		}
 	}
 	return 0;
